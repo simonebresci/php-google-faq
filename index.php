c6d997c8e79dc94df72e818867fbe31bca70ffc2
Simone Bresci <simone.bresci.05@gmail.com>
2021-03-01 15:17:39 +0100

    progetto base php + sass

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="dist/app.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <title>Google Faq</title>
+  </head>
+  <body>
+    <?php echo ciao?>
+
+  </body>
+</html>
