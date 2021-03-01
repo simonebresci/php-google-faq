5550ece1a81883caaf6fc2e97060454d0179709d
Simone Bresci <simone.bresci.05@gmail.com>
2021-03-01 17:00:47 +0100

    risolto problema CSS

--- index.php
+++ index.php
@@ -10,12 +10,14 @@ per ricreare la grafica della pagina. -->
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="dist/app.css">
+    <!-- <link rel="stylesheet" href="./dist/app.css"> -->
+    <!-- <link rel="stylesheet" href="./dist/app_copiato.css"> -->
+    <link rel="stylesheet" href="./dist/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
     <title>Google Faq</title>
   </head>
   <!-- PHP DATA -->
-    <?php
+    <!-- <?php
       $faq = [
         // Domanda risposta 1
         ["question"   =>
@@ -116,10 +118,13 @@ per ricreare la grafica della pagina. -->
            "answer"     =>
            "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."],
       ]
-     ?>
+     ?> -->
   <!-- /PHP DATA -->
   <body>
+
+     <!-- PHP FAQ FROM DB -->
     <?php
+
       foreach($faq as $qa){
         foreach($qa as $key => $value){
           if($key === 'question'){
@@ -131,6 +136,7 @@ per ricreare la grafica della pagina. -->
       }
 
      ?>
+      <!-- /PHP FAQ FROM DB -->
 
   </body>
 </html>
